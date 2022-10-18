686cf6061c72cc56f4d48929e6d90e6f2aa4f0df
SailorSnoW <snxwin@pm.me>
2022-10-18 02:58:22 +0200

    updated some website informations

--- packages/apps/webpack.serve.cjs
+++ packages/apps/webpack.serve.cjs
@@ -18,7 +18,7 @@ module.exports = merge(
     },
     plugins: [
       new HtmlWebpackPlugin({
-        PAGE_TITLE: 'Polkadot/Substrate Portal',
+        PAGE_TITLE: 'Allfeat. | Substrate Explorer',
         inject: true,
         template: path.join(__dirname, 'public/index.html')
       })
